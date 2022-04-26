c7edc547de2e037870f7fad6adf22f716e4337c7
lorenzo <lorenzo@ubuntu-18.04>
2022-04-26 17:23:48 +0200

    Runge Kutta implemented, velocitiesTicks corrected and Reset service added

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -1,26 +1,50 @@
 #include "ros/ros.h"
 #include "geometry_msgs/TwistStamped.h"
 #include "nav_msgs/Odometry.h"
+#include "project_1/Reset.h"
 
 class ComputeOdometry {
+
 public:
+
     ComputeOdometry() {
-        this->velInput=this->n.subscribe("/cmd_vel", 1000, &ComputeOdometry::eulerOdo, this);
+        
+        this->velInput=this->n.subscribe("/cmd_vel", 1000, &ComputeOdometry::eulerOdometry, this);
+        this-> resetService = this->n.advertiseService("reset", &ComputeOdometry::resetCallback, this);
+                
+        this->x0 = 0.0;
+        this->y0 = 0.0;
+        this->theta0 = 0.0;
+        this->ts0 = 0.0;
     }
+
     void mainLoop(){
-        ros::Rate loop_rate(10);
         ROS_INFO("Odometry node started\n");
-        x0=0.0;
-        y0=0.0;
-        theta0=0.0;
-        ts0=0.0;
+
+        ros::spin();
+        /*
+        ros::Rate loop_rate(10);
         while (ros::ok()) {
             ros::spinOnce();
             loop_rate.sleep();
         }
+        */
     }
-    void eulerOdo(const geometry_msgs::TwistStamped::ConstPtr& msg) {
-        if(ts0==0.0){
+
+    double computeTimeStamp(geometry_msgs::TwistStamped::ConstPtr &msg) {
+        return msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
+    }
+
+    void publishOdometry() {
+        nav_msgs::Odometry odomMsg;
+        odomMsg.pose.pose.position.x = this -> x0;
+        odomMsg.pose.pose.position.y = this -> y0;
+        odomMsg.pose.pose.orientation.z = this -> theta0;
+        this -> odomPub.publish(odomMsg);
+    }
+
+    void eulerOdometry(const geometry_msgs::TwistStamped::ConstPtr &msg) {
+        if(ts0 == 0.0) {
             ts0 = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
         }
         else {
@@ -34,29 +58,85 @@ public:
             y0 = y;
             ts0 = ts;
 
+            // creating and publishing the odometry message
+            publishOdometry();
+            /*
             nav_msgs::Odometry odomMsg;
             odomMsg.pose.pose.position.x = this -> x;
             odomMsg.pose.pose.position.y = this -> y;
             odomMsg.pose.pose.orientation.z = this -> theta;
             this -> odomPub.publish(odomMsg);
+            */
         }
     }
-    void rungeKuttaOdo(const geometry_msgs::TwistStamped::ConstPtr& msg) {
-        
+
+    void rungeKuttaOdometry(const geometry_msgs::TwistStamped::ConstPtr &msg) {
+        // 2nd order Runge Kutta integration, theta changes during the translation
+
+        if(ts0 == 0.0) {
+            // When the first message is received, the new timestamp is saved but no
+            // odometry is performed since we only have the first value of v and omega
+            ts0 = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
+        }
+        else {
+            // starting from the second message received, the odometry is computed
+            ts = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
+            deltats = ts - ts0;
+            theta = theta0 + msg->twist.angular.z * deltats;
+            v_k = sqrt(pow(msg->twist.linear.x,2) + pow(msg->twist.linear.y,2));
+            x = x0 + v_k * deltats * cos(theta0 + (msg->twist.angular.z * deltats / 2));
+            y = y0 + v_k * deltats * sin(theta0 + (msg->twist.angular.z * deltats / 2));
+
+            // updating past values with current values
+            theta0 = theta;
+            x0 = x;
+            y0 = y;
+            ts0 = ts;
+
+            // creating and publishing the odometry message
+            publishOdometry();
+        }
     }
+
+    bool resetCallback(project_1::Reset::Request  &req, project_1::Reset::Response &res) {
+
+        res.x_old = this->x0;
+        res.y_old = this->y0;
+        res.theta_old = this->theta0;
+
+        this->x0 = req.x_new;
+        this->y0 = req.y_new;
+        this->theta0 = req.theta_new;
+
+        publishOdometry();
+
+        ROS_INFO("Old pose: (%lf,%lf%,lf) - New pose: (%lf,%lf%,lf)", 
+            res.x_old, res.y_old, res.theta_old, req.x_new, req.y_new, req.theta_new);
+
+        return true;
+    }
+
 private:
+
     ros::NodeHandle n;
 	ros::Subscriber velInput;
     ros::Publisher odomPub;
+    ros::ServiceServer resetService;
     double x, y, theta;
     double x0, y0, theta0;
     double ts0;
     double ts;
     double deltats;
+    double v_k;             // absolute value of the linear velocity
+
 };
 
 int main (int argc, char **argv) {
+
     ros::init(argc, argv, "ComputeOdometry");
+
     ComputeOdometry compOdo;
     compOdo.mainLoop();
+
+    return 0;
 }
--- src/velocitiesTicks.cpp
+++ src/velocitiesTicks.cpp
@@ -3,77 +3,105 @@
 #include "geometry_msgs/TwistStamped.h"
 
 class ComputeVelocity {
+
 public:
+
 	ComputeVelocity(){
 		this->sensorInput=this->n.subscribe("/wheel_states", 1000, &ComputeVelocity::sensorCallback,this);
 		this->velocitiesPub=this->n.advertise<geometry_msgs::TwistStamped>("/cmd_vel", 1000);
+
+        this->ticfl0=0.0;
+        this->ticfr0=0.0;
+        this->ticrr0=0.0;
+        this->ticrl0=0.0;
+        this->ts0=0.0;
 	}
+
     void mainLoop(){
-        ros::Rate loop_rate(10);
-        ticfl0=0.0;
-        ticfr0=0.0;
-        ticrr0=0.0;
-        ticrl0=0.0;
-        tswfl0=0.0;
-        tswfr0=0.0;
-        tswrr0=0.0;
-        tswrl0=0.0;
         ROS_INFO("Geometry node started\n");
+
+        ros::spin();
+        /*
+        ros::Rate loop_rate(10);
         while (ros::ok()) {
             ros::spinOnce();
             loop_rate.sleep();
         }
-        
+        */
     }
+    
     void sensorCallback(const sensor_msgs::JointState::ConstPtr& msg){
         
-        if(numMsg%intMsg==0){
-            ticfl = msg->position[0]/gearRatio;
-            tswfl = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
-            wfl = (ticfl - ticfl0)/(tswfl - tswfl0);
-            ROS_INFO("Rot fl: %f", wfl);
-            ticfl0 = ticfl;
-            tswfl0 = tswfl;
+        if(msgCount%msgInterval == 0){
+            // The velocities are computed every msgInterval number of messages received
+            // to reduce noise
 
-            ticfr = msg->position[1]/gearRatio;
-            tswfr = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
-            wfr = (ticfr - ticfr0)/(tswfr - tswfr0);
-            ROS_INFO("Rot fr: %f", wfr);
-            ticfr0 = ticfr;
-            tswfr0 = tswfr;
+            if(msgCount == 0) {
+                // When the first message is received, the first values are saved but
+                // the velocities are not computed
 
-            ticrl = msg->position[2]/gearRatio;
-            tswrl = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
-            wrl = (ticrl - ticrl0)/(tswrl - tswrl0);
-            ROS_INFO("Rot rl: %f", wrl);
-            ticrl0 = ticrl;
-            tswrl0 = tswrl;
+                ts0 = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
 
-            ticrr = msg->position[3]/gearRatio;
-            tswrr = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
-            wrr = (ticrr - ticrr0)/(tswrr - tswrr0);
-            ROS_INFO("Rot rr: %f", wrr);
-            ticrr0 = ticrr;
-            tswrr0 = tswrr;
+                ticfl0 = msg->position[0]/gearRatio;
+                ticfr0 = msg->position[1]/gearRatio;
+                ticrl0 = msg->position[2]/gearRatio;
+                ticrr0 = msg->position[3]/gearRatio;
+            }
+            else {
+                // Starting from the second iteration, velocities are computed and published
 
-            vx = (wfl+wfr+wrr+wrl)*wheelRadius/4;
-            ROS_INFO("Vel x: %f", vx);
-            vy = (-wfl+wfr+wrl-wrr)*wheelRadius/4;
-            ROS_INFO("Vel y: %f", vy);
-            wz = (-wfl+wfr-wrl+wrr)*wheelRadius/(4*(wheelRadius+halfLength));
-            ROS_INFO("W z: %f", wz);
+                ts = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
 
-            geometry_msgs::TwistStamped velMsg;
-            velMsg.twist.linear.x = this -> vx;
-            velMsg.twist.linear.y = this -> vy;
-            velMsg.twist.angular.z = this -> wz;
-            this -> velocitiesPub.publish(velMsg);
+                // front-left wheel
+                ticfl = msg->position[0]/gearRatio;
+                wfl = (ticfl - ticfl0)/(ts - ts0);
+                ROS_INFO("Rot fl: %f", wfl);
+                ticfl0 = ticfl;
+
+                // front-right wheel
+                ticfr = msg->position[1]/gearRatio;
+                wfr = (ticfr - ticfr0)/(ts - ts0);
+                ROS_INFO("Rot fr: %f", wfr);
+                ticfr0 = ticfr;
+
+                // rear-left wheel
+                ticrl = msg->position[2]/gearRatio;
+                wrl = (ticrl - ticrl0)/(ts - ts0);
+                ROS_INFO("Rot rl: %f", wrl);
+                ticrl0 = ticrl;
+
+                // rear-right wheel
+                ticrr = msg->position[3]/gearRatio;
+                wrr = (ticrr - ticrr0)/(ts - ts0);
+                ROS_INFO("Rot rr: %f", wrr);
+                ticrr0 = ticrr;
+
+                ts0 = ts;
+
+                // robot velocities
+                vx = (wfl+wfr+wrr+wrl)*wheelRadius/4;
+                ROS_INFO("Vel x: %f", vx);
+                vy = (-wfl+wfr+wrl-wrr)*wheelRadius/4;
+                ROS_INFO("Vel y: %f", vy);
+                wz = (-wfl+wfr-wrl+wrr)*wheelRadius/(4*(wheelRadius+halfLength));
+                ROS_INFO("W z: %f", wz);
+
+                // publishing cmd_vel message
+                geometry_msgs::TwistStamped velMsg;
+                velMsg.twist.linear.x = this -> vx;
+                velMsg.twist.linear.y = this -> vy;
+                velMsg.twist.angular.z = this -> wz;
+                this -> velocitiesPub.publish(velMsg);
+            }
+            
         }
 
-        ++numMsg;
+        ++msgCount;
 
     }
+
 private:
+
 	ros::NodeHandle n;
 	ros::Subscriber sensorInput;
 	ros::Publisher velocitiesPub;
@@ -84,15 +112,20 @@ private:
     double wfl, wfr, wrr, wrl;
     uint32_t ticfl, ticfr, ticrr, ticrl;
     uint32_t ticfl0, ticfr0, ticrr0, ticrl0;
-    double tswfl, tswfr, tswrr, tswrl;
-    double tswfl0, tswfr0, tswrr0, tswrl0;
+    double ts;
+    double ts0;
     double vx, vy, wz;
-    int numMsg = 0;
-    const int intMsg = 5;
+    int msgCount = 0;
+    const int msgInterval = 5;
+
 };
 
 int main (int argc, char **argv) {
+    
 	ros::init(argc, argv, "ComputeVelocitiesTick");
+
     ComputeVelocity compVel;
     compVel.mainLoop();
+
+    return 0;
 }
