c05e99334d71e6d9153431b1d6ce00fcde56328f
lorenzo <lorenzo@ubuntu-18.04>
2022-04-27 19:44:29 +0200

    Dynamic reconfigure for integration method added

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -2,6 +2,8 @@
 #include "geometry_msgs/TwistStamped.h"
 #include "nav_msgs/Odometry.h"
 #include "project_1/Reset.h"
+#include "dynamic_reconfigure/server.h"
+#include "project_1/parametersConfig.h"
 
 class ComputeOdometry {
 
@@ -9,7 +11,7 @@ public:
 
     ComputeOdometry() {
         
-        this->velInput=this->n.subscribe("/cmd_vel", 1000, &ComputeOdometry::eulerOdometry, this);
+        this->velInput=this->n.subscribe("/cmd_vel", 1000, &ComputeOdometry::callOdometryMethod, this);
         this->odomPub=this->n.advertise<nav_msgs::Odometry>("/odom", 1000);
         this-> resetService = this->n.advertiseService("reset", &ComputeOdometry::resetCallback, this);
                 
@@ -17,6 +19,12 @@ public:
         this->y0 = 0.0;
         this->theta0 = 0.0;
         this->ts0 = 0.0;
+        this->set_method = 0;       // Euler integration method is set to default
+
+
+        // dynamic reconfigure setup
+        this->f = boost::bind(&ComputeOdometry::parametersCallback, this, _1, _2);
+        this->dynServer.setCallback(f);
     }
 
     void mainLoop(){
@@ -33,10 +41,15 @@ public:
     }
 
     double computeTimeStamp(geometry_msgs::TwistStamped::ConstPtr &msg) {
+        // function that returns a timestamp variable of type double, from the
+        // variables sec and nsec of type uint32_t contained in the header of the message
+
         return msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
     }
 
     void publishOdometry() {
+        // function that creates and publishes a nav_msgs::Odometry message on topic /odom
+
         nav_msgs::Odometry odomMsg;
         odomMsg.pose.pose.position.x = this -> x0;
         odomMsg.pose.pose.position.y = this -> y0;
@@ -44,7 +57,21 @@ public:
         this -> odomPub.publish(odomMsg);
     }
 
+    void callOdometryMethod(const geometry_msgs::TwistStamped::ConstPtr &msg) {
+        // This function is executed everytime a message is received and calls the
+        // correct integration method function, according to the set_method variable
+
+        if(set_method == 0) {
+            eulerOdometry(msg);
+        }
+        else {
+            rungeKuttaOdometry(msg);
+        }
+    }
+
     void eulerOdometry(const geometry_msgs::TwistStamped::ConstPtr &msg) {
+        // 1st order Euler integration, rotation is performed after the translation
+
         if(ts0 == 0.0) {
             ts0 = msg->header.stamp.sec + msg->header.stamp.nsec * pow(10, -9);
         }
@@ -100,6 +127,7 @@ public:
     }
 
     bool resetCallback(project_1::Reset::Request  &req, project_1::Reset::Response &res) {
+        // reset the robot pose to the desired values
 
         res.x_old = this->x0;
         res.y_old = this->y0;
@@ -111,11 +139,24 @@ public:
 
         publishOdometry();
 
-        ROS_INFO("Old pose: (%lf,%lf%,lf) - New pose: (%lf,%lf%,lf)", 
+        ROS_INFO("\nOld pose: (%lf,%lf,%lf)\nNew pose: (%lf,%lf,%lf)", 
             res.x_old, res.y_old, res.theta_old, req.x_new, req.y_new, req.theta_new);
 
         return true;
     }
+
+    void parametersCallback(project_1::parametersConfig &config, uint32_t level) {
+        // integration method is changed
+        
+        if(config.set_method == 0) {  
+            this->set_method = 0;       // Euler
+            ROS_INFO("Dynamic reconfigure: integration method set to Euler");
+        }
+        else {
+            this->set_method = 1;       // Runge-Kutta
+            ROS_INFO("Dynamic reconfigure: integration method set to Runge-Kutta");
+        }
+}
 
 private:
 
@@ -123,13 +164,17 @@ private:
 	ros::Subscriber velInput;
     ros::Publisher odomPub;
     ros::ServiceServer resetService;
+
+    dynamic_reconfigure::Server<project_1::parametersConfig> dynServer;
+    dynamic_reconfigure::Server<project_1::parametersConfig>::CallbackType f;
+
     double x, y, theta;
     double x0, y0, theta0;
     double ts0;
     double ts;
     double deltats;
     double v_k;             // absolute value of the linear velocity
-
+    int set_method;         // integration method: Euler = 0 , RK = 1
 };
 
 int main (int argc, char **argv) {
