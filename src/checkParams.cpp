6a00549d692fa186054e68bfda28913ea62981ee
Lorenzo Castiglia <lorenzo.castiglia@mail.polimi.it>
2022-05-06 19:02:04 +0200

    added possibility to calculate every N messages

--- src/checkParams.cpp
+++ src/checkParams.cpp
@@ -42,7 +42,7 @@ public:
             this -> ticrl = msg->position[2];
             this -> ticrr = msg->position[3];
         }
-        else {
+        else if(this->wheelCount % 100 == 0) {
             ticfl0 = ticfl;
             ticfr0 = ticfr;
             ticrl0 = ticrl;
@@ -57,7 +57,6 @@ public:
         }
         
         ++wheelCount;
-
     }
 
     void poseSubCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
@@ -72,7 +71,7 @@ public:
             m.getRPY(roll, pitch, yaw);
             theta = yaw;
         }
-        else {
+        else if(this->poseCount % 100 == 0) {
             ts0 = ts;
             x0 = x;
             y0 = y;
@@ -95,29 +94,23 @@ public:
 
     void paramCalibration() {
 
-        if(wheelCount != 0 && poseCount != 0) {
+        if(wheelCount >= 100 && poseCount >= 100) {
 
             deltats = ts - ts0;
             wz = (theta - theta0)/deltats.toSec();
             vx = (sin(theta0)*(y-y0)+cos(theta0)*(x-x0))/deltats.toSec();
             vy = (cos(theta0)*(y-y0)-sin(theta0)*(x-x0))/deltats.toSec();
+            ROS_INFO("vx %f | vy %f | wz %f", vx, vy, wz);
 
-            ROS_INFO("R: %f | N: %f", wheelRad, tickRes);
             if (paramCount%2 == 0) {
                 tempValue = (wz*deltats.toSec()*gearRatio*tickRes*(halfLength + halfWidth)*4)/(2*M_PI*(-(ticfl-ticfl0)+(ticfr-ticfr0)-(ticrl-ticrl0)+(ticrr-ticrr0)));
-                if(wheelRad<tempValue)
-                    wheelRad -= 0.001;
-                else
-                    wheelRad += 0.001;
+                ROS_INFO("tempValue R: %f", tempValue);
             }
             if (paramCount%2 == 1) {
                 tempValue = ((-(ticfl-ticfl0)+(ticfr-ticfr0)-(ticrl-ticrl0)+(ticrr-ticrr0))*2*M_PI*wheelRad)/(deltats.toSec()*gearRatio*wz*(halfLength + halfWidth)*4);
-                if(tickRes<tempValue)
-                    tickRes -= 0.001;
-                else
-                    tickRes += 0.001;
+                ROS_INFO("tempValue N: %f", tempValue);
             }
-            ROS_INFO("R: %f | N: %f, tempValue: %f", wheelRad, tickRes, tempValue);
+            //ROS_INFO("R: %f | N: %f, tempValue: %f", wheelRad, tickRes, tempValue);
 
             ++paramCount;
         }
